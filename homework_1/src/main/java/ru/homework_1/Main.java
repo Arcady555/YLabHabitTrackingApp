f2ba7b688df0bacbbed331c8b494810d228701d2
arcady555 <parfenov7333@gmail.com>
2024-10-08 06:51:24 +0300

    initial commit

---
+++ homework_1/src/main/java/ru/homework_1/Main.java
@@ -0,0 +1,10 @@
+package ru.homework_1;
+
+import ru.homework_1.server.ServerClass;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO!!!" + System.lineSeparator() + "Welcome!");
+        ServerClass server = new ServerClass();
+    }
+}
---
+++ homework_1/src/main/java/ru/homework_1/server/ServerClass.java
@@ -0,0 +1,9 @@
+package ru.homework_1.server;
+
+import java.io.IOException;
+
+public class ServerClass {
+    public void run() throws IOException, InterruptedException {
+        System.out.println("Hello!");
+    }
+}
