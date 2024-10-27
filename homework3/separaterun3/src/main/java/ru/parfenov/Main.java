d97fb428a3fdc30dd194769a1cd26e281e17d9f6
arcady555 <parfenov7333@gmail.com>
2024-10-27 17:43:17 +0300

    Блок separaterun3

--- homework3/separaterun3/src/main/java/ru/parfenov/Main.java
+++ homework3/separaterun3/src/main/java/ru/parfenov/Main.java
@@ -4,8 +4,12 @@ import liquibase.exception.LiquibaseException;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
+
+/**
+ * В отдельный блок выведены скрипты liquibase и регулярная рассылка по емайл.
+ * Загрузку таблиц в БД можно было сделать через плагин в соседнем блоке.
+ * Но тестконтейнеры там же требуют создания объекта liquibase. Тогда плагин будет усложнять задачу.
+ */
 
 public class Main {
     public static void main(String[] args) throws SQLException, IOException, LiquibaseException, ClassNotFoundException {
--- homework3/separaterun3/src/main/java/ru/parfenov/RegularRequest.java
+++ homework3/separaterun3/src/main/java/ru/parfenov/RegularRequest.java
@@ -2,57 +2,72 @@ package ru.parfenov;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-/*import ru.parfenov.server.emailsend.SendViaEmail;
-import ru.parfenov.server.model.Habit;
-import ru.parfenov.server.model.User;
-import ru.parfenov.server.service.HabitService;
-import ru.parfenov.server.service.UserService;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-import javax.mail.MessagingException; */
-import java.util.List;
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
 /**
- * Класс формирует данные для отправки сообщений на емайлы юзеров. Ежедневно.
- * С напоминанием, какие привычки им необходимо выполнить сегодня
+ * Класс делает регулярный HTTP запрос.
  */
 @Slf4j
 @RequiredArgsConstructor
 public class RegularRequest {
-   // private final UserService userService;
-  //  private final HabitService habitService;
+    private final CloseableHttpClient httpClient = HttpClients.createDefault();
 
-    public void run() { //} throws MessagingException {
+    public void run() {
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
-              /*  List<User> userList = userService.findAll();
-                for (User user : userList) {
-                    List<Habit> todayHabitList = habitService.todayPerforms(user);
-                    if (todayHabitList.isEmpty()) {
-                        continue;
-                    } else {
-                        StringBuilder builder = new StringBuilder(
-                                        "Hello dear!!!!" +
-                                                System.lineSeparator() +
-                                                "These are your habits today :" +
-                                                System.lineSeparator()
-                                );
-                        for (Habit habit : todayHabitList) {
-                            builder.append(habit.getId()).append(" ").append(habit.getName());
-                        }
-                        SendViaEmail sendViaEmail = new SendViaEmail(
-                                user.getEmail(), "Remind your habit performs today", builder.toString()
-                        );
-                        try {
-                            sendViaEmail.run();
-                        } catch (MessagingException e) {
-                            throw new RuntimeException(e);
-                        }
+                RegularRequest obj = new RegularRequest();
+
+                try {
+                    try {
+                        obj.sendGet();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
                     }
-                } */
+                } finally {
+                    try {
+                        obj.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
-        }, 0, 24 * 60 * 60 * 1000);
+        }, 60 * 60 * 1000, 24 * 60 * 60 * 1000);
+    }
+
+    private void close() throws IOException {
+        httpClient.close();
+    }
+
+    private void sendGet() throws Exception {
+        HttpGet request = new HttpGet("http://localhost:8080/ht/remind-today-perform-via-email");
+
+        request.addHeader("name", "admin@YLabHabitApp.com");
+        request.addHeader("password", "123");
+
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+
+            System.out.println(response.getStatusLine().toString());
+
+            HttpEntity entity = response.getEntity();
+            Header headers = entity.getContentType();
+            System.out.println(headers);
+
+            if (entity != null) {
+                String result = EntityUtils.toString(entity);
+                System.out.println(result);
+            }
+
+        }
     }
 }
