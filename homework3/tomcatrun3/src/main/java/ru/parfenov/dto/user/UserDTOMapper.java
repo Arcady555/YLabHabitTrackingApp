a99fc79a921d24dcbb25f0271f8a082906f48ff0
arcady555 <parfenov7333@gmail.com>
2024-11-01 23:41:01 +0300

    Удалил лишние папки с ДЗ-1 и ДЗ-2

--- homework3/tomcatrun3/src/main/java/ru/parfenov/dto/user/UserDTOMapper.java
+++
@@ -1,7 +0,0 @@
-package ru.parfenov.dto.user;
-
-import org.mapstruct.Mapper;
-
-@Mapper
-public interface UserDTOMapper {
-}
--- homework3/tomcatrun3/src/main/java/ru/parfenov/service/LogService.java
+++
@@ -1,9 +0,0 @@
-package ru.parfenov.service;
-
-import ru.parfenov.model.LogRecord;
-
-import java.util.List;
-
-public interface LogService {
-    List<LogRecord> findByDateTime(String dateTimeFrom, String dateTimeTo);
-}
