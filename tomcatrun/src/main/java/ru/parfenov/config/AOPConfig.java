4887be78ba59549101b924ef0b4215296a3971bc
arcady555 <parfenov7333@gmail.com>
2024-11-05 10:43:02 +0300

    AOP

--- tomcatrun/src/main/java/ru/parfenov/config/AOPConfig.java
+++ tomcatrun/src/main/java/ru/parfenov/config/AOPConfig.java
@@ -1,11 +1,9 @@
 package ru.parfenov.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-//@Configuration
-//@ComponentScan(basePackages = "ru.parfenov")
-//@EnableAspectJAutoProxy
+@Configuration
+@EnableAspectJAutoProxy
 public class AOPConfig {
 }
