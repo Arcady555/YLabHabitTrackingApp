7a8441fa3b8559802ddc37d88c6a3ee2a0acda87
arcady555 <parfenov7333@gmail.com>
2024-11-11 10:57:55 +0300

    Тесты репозиториев

--- habittracker/src/test/java/ru/parfenov/repository/UserRepositoryTest.java
+++ habittracker/src/test/java/ru/parfenov/repository/UserRepositoryTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -22,9 +21,6 @@ class UserRepositoryTest {
     @Container
     @ServiceConnection
     static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15");
-
-    @Autowired
-    MockMvc mockMvc;
 
     @Autowired
     UserRepository userRepository;
